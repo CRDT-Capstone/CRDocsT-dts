52cfbd67868ce7442b13c43a4e721ab3777b8b9e
nignig0 <tanitoluwaadebayo@gmail.com>
2026-03-20 11:56:44 +0000

    made presence message it's own type

--- src/types/Presence.ts
+++ src/types/Presence.ts
@@ -3,20 +3,25 @@ import { BaseMessage, MessageType } from "./Message.js";
 export enum PresenceMessageType {
     CURSOR,
     UPDATE,
+    JOIN
 }
 
-export interface BasePresenceMessage<T extends PresenceMessageType = PresenceMessageType>
-    extends BaseMessage<MessageType.PRESENCE> {
-    type: T;
+export interface BasePresenceMessage<
+  T extends PresenceMessageType = PresenceMessageType
+> extends Omit<BaseMessage<MessageType.PRESENCE>, "documentID"> {
+  type: T;
+  documentID?: string //had to make document ID optional... things we're getting hard to work with
 }
 
 export interface PresenceCursorMessage extends BasePresenceMessage<PresenceMessageType.CURSOR> {
     pos: number;
 }
 
-export interface PresenceUpdateMessage extends BasePresenceMessage<PresenceMessageType.UPDATE> {}
+export interface PresenceUpdateMessage extends BasePresenceMessage<PresenceMessageType.UPDATE> { };
 
-export type PresenceMessage = PresenceCursorMessage | PresenceUpdateMessage;
+export interface PresenceJoinMessage extends BasePresenceMessage<PresenceMessageType.JOIN> { };
+
+export type PresenceMessage = PresenceCursorMessage | PresenceUpdateMessage | PresenceJoinMessage;
 
 export const makePresenceMsg = <T extends PresenceMessage>(msg: Omit<T, "msgType">): BasePresenceMessage => {
     return {
